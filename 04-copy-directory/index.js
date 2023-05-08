404fe381b061a5dfd7d36138fd5f12e4123ddb63
ElenaDegtiarenko <elena.degtiarenko@intexsoft.by>
2023-05-08 17:07:51 +0300

    feat: add 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs = require('fs/promises');
+const path = require('path');
 
+const originalFolder = 'files';
+const copyFolder = 'files-copy';
+const copyFolderPath = path.join(__dirname, copyFolder);
+const originalFolderPath = path.join(__dirname, originalFolder);
+
+const copyDir = async () => {
+  try {
+    await fs.rm(copyFolderPath, { recursive: true, force: true });
+    await fs.mkdir(copyFolderPath, { recursive: true });
+    const files = await fs.readdir(originalFolderPath);
+
+    for (let file of files) {
+      const source = path.join(originalFolderPath, file);
+      const destination = path.join(copyFolderPath, file);
+      await fs.copyFile(source, destination);
+    }
+    console.log('Folder has been copied.');
+  } catch (err) {
+    console.log(`Error: ${err.message}`);
+  }
+};
+
+copyDir();
